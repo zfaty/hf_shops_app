fa6f1cb5434705aceca4a02e66efdb9458294587
Fatima Zahare <fz.elhammoumi@gmail.com>
2018-01-26 18:32:08 +0000

    start home page

---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types';
+
+class Home extends Component {
+
+  render() {
+    const props = this.props;
+    return (
+      <div className="container">
+      <p>Hommmiii</p>
+      </div>
+    )
+  }
+}
+
+export default Home
